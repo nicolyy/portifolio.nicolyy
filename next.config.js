1ade6a57fc920f49566a8b75402345837baae665
github.com/nicolyy <nicollyoliveira003@gmail.com>
2024-01-06 00:45:03 -0300

    Atualização dos arquivos padrões

--- next.config.js
+++ next.config.js
@@ -2,3 +2,32 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+module.exports = {
+    // remove modules you don't need
+    modules: [
+      'virtual',
+      'keyboard',
+      'mousewheel',
+      'navigation',
+      'pagination',
+      'scrollbar',
+      'parallax',
+      'zoom',
+      'controller',
+      'a11y',
+      'history',
+      'hash-navigation',
+      'autoplay',
+      'thumbs',
+      'free-mode',
+      'grid',
+      'manipulation',
+      'effect-fade',
+      'effect-cube',
+      'effect-flip',
+      'effect-coverflow',
+      'effect-creative',
+      'effect-cards',
+    ],
+  };
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,116 @@
+''
+
 import Image from 'next/image'
-import styles from './page.module.css'
+import Link from 'next/link'
+import '../components/styles/style.scss'
+
+import Cabecalho from '../components/cabecalho/cabecalho-social-medias'
+import Rodape from '@/components/rodape/rodape'
+import Carrossel from '@/components/carrossel/carrossel'
+import Formulario from '@/components/formulario/formulario'
+import Modal from '@/components/modal/modal'
+
+
+
+import Html from '../../public/html.svg'
+import Css from '../../public/Css.svg'
+import JavaScript from  '../../public/JavaScript.svg'
+import React from  '../../public/React.svg'
+import Figma from '../../public/Figma.svg'
+import Github from '../../public/Github.svg'
+import Block from '../../public/Block.svg'
+
+
+
 
 export default function Home() {
+ 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
+    <Cabecalho/>
+       
+        
+        <main className='container-principal'>
+            <div className='container-titulo'>
+                <h1 className='titulo1'>Olá!, meu nome é</h1>
+                <h1>Nicoly Oliveira</h1>
+                <p className='subtitulo'>Eu sou Desenvolvedora em processo...</p>
+            </div>
+            <div className='botoes-titulo'>
+                <Link className='btn-conhecer' href="#"> Conhecer</Link>
+                <Link className='btn-curriculo' href="#"> Curriculo</Link>
+            </div>
+        </main>
+        <section className='sobre-mim'>
+          <div className='sobre-mim-texto'>
+              <h2>Sobre Mim</h2>
+              <p>Prazer, Nicoly! Sou estudante de análise e desenvolvimento de sistenas, 
+              morando em São Paulo. Eu me interesso pela área desde os meus 15 anos, 
+              quando tive o meu primeiro contato por meio do técnico, através do curso
+              consegui ter a certeza que era o que eu queria. Sou apaixonada por Design
+              Digital e Desenvolvimento Web, tenho como objetivo seguir com a carreira 
+              de Front End e User Experience, mas disposta a conhecer novos caminhos. 
+              Há um ano, consegui minha bolsa de 100% através do Prouni, onde tive a 
+              oportunidade de iniciar a minha graduação, agora, estou em busca de 
+              uma oportunidade para desenvolver minhas habilidades. <button className='texto-curriculo'>Meu Curriculo</button></p>
+            </div>
+            <Modal/>
+            
+        </section>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <section className='habilidades'>
+            <h2>Minhas habilidades</h2>
+            <div className='container-habilidades'>
+                <div className='habilidade-item'>
+                    <Image className='hover-zoom' src={Html} alt=""/>
+                </div>
+                <div className='habilidade-item'>
+                    <Image className='hover-zoom'  src={Css} alt=""/>
+                </div>
+                <div className='habilidade-item'>
+                    <Image className='hover-zoom'  src={JavaScript} alt=""/>
+                </div>
+                <div className='habilidade-item'>
+                    <Image className='hover-zoom'  src={React} alt=""/>
+                </div>
+                <div className='habilidade-item'>
+                    <Image className='hover-zoom'  src={Figma} alt=""/>
+                </div>
+                <div className='habilidade-item'>
+                    <Image className='hover-zoom'  src={Github} alt=""/>
+                </div>
+                <div className='habilidade-item'>
+                    <Image className='hover-zoom'  src={Block} alt=""/>
+                </div>
+                <div className='habilidade-item'>
+                    <Image className='hover-zoom'  src={Block} alt=""/>
+                </div>
+                <div className='habilidade-item'>
+                    <Image className='hover-zoom'  src={Block} alt=""/>
+                </div>
+                <div className='habilidade-item'>
+                    <Image className='hover-zoom'  src={Block} alt=""/>
+                </div>
+                <div className='habilidade-item'>
+                    <Image className='hover-zoom'  src={Block} alt=""/>
+                </div>
+            </div>
+        </section>
+        <section className='projetos-desenvolvidos'>
+            <h2>Projetos Desenvolvidos</h2>
+            <Carrossel/>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+            <div className='nav-projetos'>
+              <Link className='nav-projetos-item' href="/pages/meusProjetos" >Acessar Portifólio</Link>
+              <Link className='nav-projetos-item' href="/pages/meusCertificados">Certificados</Link>
+            </div>
+        </section>
+        
+       <Formulario/>
+       <Rodape/>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
+    
   )
 }
