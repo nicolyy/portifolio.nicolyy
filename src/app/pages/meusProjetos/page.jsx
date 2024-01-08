332b0f48b1d2b5ef4f9125ad36edbeaf9f27446e
github.com/nicolyy <nicollyoliveira003@gmail.com>
2024-01-07 23:04:19 -0300

    Criando a pagina de projetos

--- src/app/pages/meusProjetos/page.jsx
+++ src/app/pages/meusProjetos/page.jsx
@@ -1,6 +1,47 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import Cabecalho from '@/components/cabecalho/cabecalho-principal'
+import Rodape from '@/components/rodape/rodape'
+
 export default function meusProjetos(){
     return(
         <>
+        <Cabecalho/>
+        <div>
+            <h1>Meus Projetos</h1>
+        </div>
+        <div className='projetos'>
+            <div className='projeto-item'>
+               <Link href='https://www.youtube.com/'></Link>
+               <div className='github-rep'>
+                <Link href='https://www.google.com.br/?hl=pt-BR'></Link>
+               </div>
+            </div>
+            <div className='projeto-item'>
+               <Link href='#'></Link>
+            </div>
+            <div className='projeto-item'>
+               <Link href='#'></Link>
+            </div>
+            <div className='projeto-item'>
+               <Link href='#'></Link>
+            </div>
+            <div className='projeto-item'>
+               <Link href='#'></Link>
+            </div>
+            <div className='projeto-item'>
+               <Link href='#'></Link>
+            </div>
+            <div className='projeto-item'>
+               <Link href='#'></Link>
+            </div>
+            <div className='projeto-item'>
+               <Link href='#'></Link>
+            </div>
+
+        </div>
+        <Rodape/>
         </>
     )
 }
