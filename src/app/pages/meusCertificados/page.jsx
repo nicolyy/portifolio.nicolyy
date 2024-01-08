6f48462de8cc0f05d242443c00293cf179988dd7
github.com/nicolyy <nicollyoliveira003@gmail.com>
2024-01-07 23:04:28 -0300

    Adicionando os certificados

--- src/app/pages/meusCertificados/page.jsx
+++ src/app/pages/meusCertificados/page.jsx
@@ -6,6 +6,15 @@ import Cabecalho from '@/components/cabecalho/cabecalho-principal'
 import Seta from '../../../../public/Seta.svg'
 
 import Certificado1 from '../../../../public/Certificados/logica-de-programacao/Logica de Programação - FIAP - NANO COURSE.png'
+import Certificado2 from '../../../../public/Certificados/desenvolvimento-web/Nicoly Oliveira Santos - Curso Git e GitHub_ repositório, commit e versões - Alura-1.png'
+import Certificado3 from '../../../../public/Certificados/desenvolvimento-web/Nicoly Oliveira Santos - Curso HTML e CSS_ responsividade com mobile-first - Alura-1.png'
+import Certificado4 from '../../../../public/Certificados/desenvolvimento-web/Nicoly Oliveira Santos - Formação A partir do zero_ HTML e CSS para projetos web - Alura-1.png'
+import Certificado5 from '../../../../public/Certificados/ingles/Certificado SKILL IDIOMAS-1.png'
+import Certificado6 from '../../../../public/Certificados/ingles/English for Career Development-1.png'
+import Certificado7 from '../../../../public/Certificados/logica-de-programacao/Nicoly Oliveira Santos - Curso Lógica de programação_ comece em lógica com o jogo Pong e JavaScript - Alura-1.png'
+import Certificado8 from '../../../../public/Certificados/ux-ui/usp ui.ux-1.png'
+import Certificado9 from '../../../../public/Certificados/ux-ui/Design Thinking FIAP.png'
+import Certificado10 from '../../../../public/Certificados/ux-ui/User-Experience.png'
 
 export default function meusCertificados(){
     return(
@@ -22,9 +31,16 @@ export default function meusCertificados(){
                 <Image src={Seta} alt=''width={80} height={100}/>
             </summary>
             <div className='summary-open'>
-                <div>
-                    <Image src={Certificado1} alt='' width={500} height={300}/>
+                <div className='certificado-img'>
+                    <Image src={Certificado2} alt='' width={600} height={400}/>
                 </div>
+                <div className='certificado-img'>
+                    <Image src={Certificado3} alt='' width={600} height={400}/>
+                </div>
+                <div className='certificado-img'>
+                    <Image src={Certificado4} alt='' width={600} height={400}/>
+                </div>
+               
             </div>
         </details>
 
@@ -33,7 +49,16 @@ export default function meusCertificados(){
             <Image src={Seta} alt=''width={80} height={100}/>
             </summary>
             <div className='summary-open'>
-                <p></p>
+                <div className='certificado-img'>
+                    <Image src={Certificado8} alt='' width={600} height={400}/>
+                </div>
+                <div className='certificado-img'>
+                    <Image src={Certificado9} alt='' width={600} height={400}/>
+                </div>
+                <div className='certificado-img'>
+                    <Image src={Certificado10} alt='' width={600} height={400}/>
+                </div>
+                
             </div>
         </details>
 
@@ -42,7 +67,13 @@ export default function meusCertificados(){
             <Image src={Seta} alt=''width={80} height={100}/>
             </summary>
             <div className='summary-open'>
-                <p></p>
+                <div className='certificado-img'>
+                    <Image src={Certificado1} alt='' width={600} height={400}/>
+                </div>
+                <div className='certificado-img'>
+                    <Image src={Certificado7} alt='' width={600} height={400}/>
+                </div>
+             
             </div>
         </details>
 
@@ -51,7 +82,13 @@ export default function meusCertificados(){
             <Image src={Seta} alt=''width={80} height={100}/>
             </summary>
             <div className='summary-open'>
-                <p></p>
+                <div className='certificado-img'>
+                    <Image className='certificado-skill' src={Certificado5} alt='' width={400} height={600}/>
+                </div>
+                <div className='certificado-img'>
+                    <Image src={Certificado6} alt='' width={600} height={400}/>
+                </div>
+                
             </div>
         </details>
 
