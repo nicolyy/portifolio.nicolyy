5cfa818cfa881a91a2d127759b7efa5fd831b2d2
github.com/nicolyy <nicollyoliveira003@gmail.com>
2024-02-18 21:18:53 -0300

    Nova pagina obrigado

--- src/app/pages/obrigado/page.jsx
+++ src/app/pages/obrigado/page.jsx
@@ -1,7 +1,20 @@
-export default function Obrigado (){
+import Image from 'next/image'
+import Link from 'next/link'
+
+import '@/components/styles/style.scss'
+import Obrigado from '../../../../public/obrigado.svg'
+
+export default function ObrigadoPage (){
     return(
         <>
-        <p>Obirgada!!</p>
+        <main className='container-obrigado'>
+            <div className='texto-obrigado'>
+            <Image src={Obrigado} alt='' width={400} height={200}/>
+            <p>Obrigada pelo envio!</p>
+            <p>Em breve entrarei em contato!</p>
+            <Link className='obrigado-home' href="#">Voltar para HOME</Link>
+            </div>
+        </main>
         </>
     )
 }
